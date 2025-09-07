00aef92f24f4a44f3b866e70e1e1a866eadf6036
KhaiTrinhVan <trinhvankhai61@gmail.com>
2025-09-07 10:07:16 +0700

    Update Motorbike.cpp

--- Motorbike.cpp
+++ Motorbike.cpp
@@ -1,1 +1,19 @@
+#include "Motorbike.h"
 
+// Constructor implementation
+Motorbike::Motorbike(std::string& brand, std::string& model, std::string& color,
+                     int engineSize, int yearMade, std::string& licensePlate, 
+                     std::string& location, double dailyRate, double rating,
+                     std::string& ownerName)
+    : brand(brand), model(model), color(color), engineSize(engineSize), 
+      yearMade(yearMade), licensePlate(licensePlate), location(location),
+      dailyRate(dailyRate), rating(rating), ownerName(ownerName) {}
+
+std::string Motorbike::getBrand() { return brand; }
+std::string Motorbike::getModel() { return model; }
+int Motorbike::getEngineSize() { return engineSize; }
+std::string Motorbike::getLocation() { return location; }
+
+double Motorbike::getDailyRate() { return dailyRate; }
+double Motorbike::getRating() { return rating; }
+std::string Motorbike::getOwnerName() { return ownerName; }
